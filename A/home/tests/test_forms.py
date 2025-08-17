e302b759feaf421476a89305b55fb91d4c358f5a
shahramsss <shahramsss1987@gmail.com>
2025-08-17 11:51:12 +0330

    add from refgistration tests

--- A/home/tests/test_forms.py
+++ A/home/tests/test_forms.py
@@ -1,2 +1,52 @@
+from django.test import TestCase
+from home.forms import UserRegistrationForm
+from django.contrib.auth.models import User
 
 
+class TestRegistrationForm(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(
+            username="s", email="s1@email.com", password="Test12345!"
+        )
+
+    def test_valid_data(self):
+        form = UserRegistrationForm(
+            data={
+                "username": "s2",
+                "email": "s2@email.com",
+                "password1": "Test12345!",
+                "password2": "Test12345!",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_empty_form(self):
+        form = UserRegistrationForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 4)
+
+    def test_exist_email(self):
+        form = UserRegistrationForm(
+            data={
+                "username": "s3",
+                "email": "s1@email.com",  # ایمیل تکراری
+                "password1": "Test12345!",
+                "password2": "Test12345!",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.has_error("email"))
+
+    def test_unmatch_password(self):
+        form = UserRegistrationForm(
+            data={
+                "username": "s4",
+                "email": "s4@email.com",
+                "password1": "Test12345!",
+                "password2": "Wrong12345!",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.has_error)
--- A/home/views.py
+++ A/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .froms import UserRegistrationForm
+from .forms import UserRegistrationForm
 from django.contrib.auth.models import User
 
 
