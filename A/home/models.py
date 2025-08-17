0ecb1a77a10c980debaaaf5b508f7a6c34f2a9fe
shahramsss <shahramsss1987@gmail.com>
2025-08-17 12:05:30 +0330

    add test model writer

--- A/home/models.py
+++ A/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Writer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Writer"
+        verbose_name_plural = "Writers"
--- A/home/tests/test_models.py
+++ A/home/tests/test_models.py
@@ -1,2 +1,14 @@
+from django.test import TestCase
+from home.models import Writer
 
 
+class TestWriterModel(TestCase):
+    def test_model_str(self):
+        writer = Writer.objects.create(
+            first_name="sss",
+            last_name="shah",
+            email="shah@email.com",
+            password="shah",
+        )
+        self.assertEqual(str(writer) , 'sss shah')
+    
