57dc8ada4d71491aeaf4d4d5a5feecdffe7fdaf5
shahramsss <shahramsss1987@gmail.com>
2025-08-17 12:09:22 +0330

    add model_bakery for create objects

--- A/home/tests/test_models.py
+++ A/home/tests/test_models.py
@@ -1,14 +1,19 @@
 from django.test import TestCase
 from home.models import Writer
+from model_bakery import baker
 
 
 class TestWriterModel(TestCase):
     def test_model_str(self):
-        writer = Writer.objects.create(
+        # writer = Writer.objects.create(
+        #     first_name="sss",
+        #     last_name="shah",
+        #     email="shah@email.com",
+        #     password="shah",
+        # )
+        writer = baker.make(
+            Writer,
             first_name="sss",
             last_name="shah",
-            email="shah@email.com",
-            password="shah",
         )
-        self.assertEqual(str(writer) , 'sss shah')
-    
+        self.assertEqual(str(writer), "sss shah")
