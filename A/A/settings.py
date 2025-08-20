c593fb02ed2b39c6e67ccb198bfc8f3a761ff394
shahramsss <shahramsss1987@gmail.com>
2025-08-20 11:42:42 +0330

    add BaseAPIRequestLog for model log

--- A/A/settings.py
+++ A/A/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # local apps
-    "home",
+    "home.apps.HomeConfig",
+    "tracking.apps.TrackingConfig",
 ]
 
 MIDDLEWARE = [
---
+++ A/tracking/models.py
@@ -0,0 +1,5 @@
+from .basemodels import BaseAPIRequestLog
+
+
+class APIRequestLog(BaseAPIRequestLog):
+    pass
