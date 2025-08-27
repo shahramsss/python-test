98718953b9eba7ad000ec781a60a466f7545a04d
shahramsss <shahramsss1987@gmail.com>
2025-08-27 11:15:22 +0330

    override should_log in HomeView

--- A/tracking/views.py
+++ A/tracking/views.py
@@ -5,8 +5,10 @@ from .mixins import LoggingMixin
 
 
 class HomeView(LoggingMixin, APIView):
-    logging_methods = ['GET' , 'POST']
+    # logging_methods = ['GET' , 'POST']
+
+    def should_log(self, response, request):
+        return response.status_code > 400
+
     def get(self, request):
         return Response("hello , hi ")
-    
-   
